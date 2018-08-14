9e1b5cbcc8d16907ebfc7c62c78ec82863139a57
Faruk Heynemann <info@hennige-it.de>
2018-08-14 23:33:36 +0200

    moved elo class from team to player

--- player/models.py
+++ player/models.py
@@ -21,30 +21,46 @@ class Player(models.Model):
             ))
 
 
-def expected(A, B):
-    """ Calculate expected score of A in a match against B
-    :param A: Elo rating for player A
-    :param B: Elo rating for player B
-    """
-    return 1 / (1 + 10 ** ((B - A) / 400))
+class Elo:
+    def __init__(self):
+        self.elo = 1000
 
+    def new_result(self, enemy_elo, goal_diff):
+        exp = self.expected(enemy_elo)
+        self.new_elo(exp, goal_diff)
 
-def elo(old, exp, score, counted_games):
-    """ Calculate the new Elo rating for a player
-    :param old: The previous Elo rating
-    :param exp: The expected score for this match
-    :param score: Goal difference
-    """
-    if score <= 0:
-        score = 0
-    else:
-        score = 1 + score / 10
+    def expected(self, enemy_elo):
+        return __class__._expected(self.elo, enemy_elo)
 
-    if old > 2400:
-        k = 10
-    elif counted_games < 40:
-        k = 40
-    else:
-        k = 20  # default
+    def new_elo(self, exp, goal_diff):
+        self.elo = __class__._new_elo(self.elo, exp, goal_diff)
 
-    return old + k * (score - exp)
+    @staticmethod
+    def mapper(value, minFrom, maxFrom, minTo, maxTo):
+        return minTo + (maxTo - minTo) * ((value - minFrom) /
+                                          (maxFrom - minFrom))
+
+    @staticmethod
+    def _expected(A, B):
+        """ Calculate expected score of A in a match against B
+        :param A: Elo rating for player A
+        :param B: Elo rating for player B
+        """
+        return 1 / (1 + 10 ** ((B - A) / 400))
+
+    @staticmethod
+    def _new_elo(old, exp, goal_diff):
+        """ Calculate the new Elo rating for a player
+        :param old: The previous Elo rating
+        :param exp: The expected score for this match
+        :param goal_diff: Goal difference
+        """
+        if goal_diff == 0:
+            score = 0.5  # normally not possible
+        elif goal_diff < 0:
+            score = 0 + goal_diff
+        else:
+            score = 1 + goal_diff / 10
+
+        k = __class__.mapper(old, 0, 2400, 40, 10)
+        return old + k * (score - exp)
--- team/models.py
+++ team/models.py
@@ -18,6 +18,7 @@ class Team(models.Model):
         return self.tscore
 
     def get_team_rating(self):
+        """ Calculates the team strength out of the Player Elo """
         return sum([x.get_player_rating() for x in self.players.all()])
 
     def __str__(self):
@@ -29,48 +30,3 @@ class Team(models.Model):
                 ", ".join([x.nick for x in self.players.all()])
                 )
             )
-
-
-class Elo:
-    def __init__(self):
-        self.elo = 1000
-
-    def new_result(self, enemy_elo, goal_diff):
-        exp = self.expected(enemy_elo)
-        self.new_elo(exp, goal_diff)
-
-    def expected(self, enemy_elo):
-        return __class__._expected(self.elo, enemy_elo)
-
-    def new_elo(self, exp, goal_diff):
-        self.elo = __class__._new_elo(self.elo, exp, goal_diff)
-
-    @staticmethod
-    def mapper(value, minFrom, maxFrom, minTo, maxTo):
-        return minTo + (maxTo - minTo) * ((value - minFrom) /
-                                          (maxFrom - minFrom))
-
-    @staticmethod
-    def _expected(A, B):
-        """ Calculate expected score of A in a match against B
-        :param A: Elo rating for player A
-        :param B: Elo rating for player B
-        """
-        return 1 / (1 + 10 ** ((B - A) / 400))
-
-    @staticmethod
-    def _new_elo(old, exp, goal_diff):
-        """ Calculate the new Elo rating for a player
-        :param old: The previous Elo rating
-        :param exp: The expected score for this match
-        :param goal_diff: Goal difference
-        """
-        if goal_diff == 0:
-            score = 0.5  # normally not possible
-        elif goal_diff < 0:
-            score = 0 + goal_diff
-        else:
-            score = 1 + goal_diff / 10
-
-        k = __class__.mapper(old, 0, 2400, 40, 10)
-        return old + k * (score - exp)
