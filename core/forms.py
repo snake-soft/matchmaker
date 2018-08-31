24c12ca60032418915ff64cac795bc032b9d3fcb
Frank Hennige <info@hennige-it.de>
2018-08-31 19:26:29 +0200

    from ant to bootstrap compatibility

--- core/forms.py
+++ core/forms.py
@@ -5,7 +5,7 @@ class TimeRangeForm(forms.Form):
     def __init__(self, request):
         super().__init__()
         self.fields["from"] = forms.DateField(
-            widget=forms.DateInput(attrs={"data-toggle": "datepicker"}),
+            widget=forms.DateInput(attrs={"data-toggle": "datepicker", }),
             initial=request.session["from"],
             )
         self.fields["to"] = forms.DateField(
