eabaf939e13094a68a4ee0581abb347fa0cd3db7
snake-soft <info@snake-soft.com>
2018-09-05 18:27:10 +0200

    use last player and count in matchmaker

--- matchmaker/views.py
+++ matchmaker/views.py
@@ -10,16 +10,22 @@ from player.models import Player
 class MatchmakerView(LoginRequiredMixin, View):
     def get(self, request):
         context = {}
-        request.session['last_players'] = request.GET.getlist('players')
-        request.session['last_count'] = request.GET.get('count')
+
+        if 'players' in request.GET:
+            request.session['last_players'] = request.GET.getlist('players')
+        if 'count' in request.GET:
+            request.session['last_count'] = request.GET.get('count')
+
         initial_count = int(request.session['last_count']) \
             if request.session['last_count'] else 2
         initial_players = request.session['last_players'] \
             if len(request.session['last_players']) else ''
+
         form = MatchmakerForm(request, initial={
             'count': initial_count,
             'players': initial_players,
             })
+
         if 'players' and 'count' in request.GET:
             players = [Player.objects.get(pk=x, owner=request.user)
                        for x in request.GET.getlist('players')]
@@ -29,7 +35,9 @@ class MatchmakerView(LoginRequiredMixin, View):
             if len(request.GET.getlist('players')) \
                     < int(request.GET.get('count')):
                 form.errors['error'] = 'Choose more players !'
+
         context["matchmaker_form"] = form
+
         return render(
             request,
             template_name="matchmaker/matchmaker_form.html",
