850148d745b0d8841cf4cee6ee819136554603e8
snake-soft <info@snake-soft.com>
2018-09-05 17:24:41 +0200

    wsgi working

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,8 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from config.settings import production
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", production)
-# "config.settings.production"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 application = get_wsgi_application()
