c682a6755cb4aa69f718dbcf32116e436b3477e0
Snake-Soft <40820005+snake-soft@users.noreply.github.com>
2020-01-10 16:06:40 +0100

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -115,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-en'
+LANGUAGE_CODE = 'en-US'
 
-TIME_ZONE = 'Europe/Berlin'
+# TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
